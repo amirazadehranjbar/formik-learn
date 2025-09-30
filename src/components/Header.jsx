1d488650b2f8a67c1597829e92f64a8e5316c7fb
amira <amirazadehranjbar@gmail.com>
2025-09-30 11:33:28 -0700

    react router 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,15 +8,23 @@ function Header() {
     return (
         <>
             <header className="w-full h-auto flex justify-center items-center gap-2 mb-2">
+
+
                 <div
                     className="w-1/2 bg-blue-800 h-full border-gray-700 border-2 rounded-b-md text-center cursor-pointer"
                 onClick={()=>navigate("/")}>
                     <p className="text-gray-300 font-bold text-xl p-1">full form</p>
                 </div>
+
+
                 <div
                     className="w-1/2 bg-blue-800 h-full border-gray-700 border-2 rounded-b-md text-center cursor-pointer" onClick={() => navigate("/auth")}>
                     <p className="text-gray-300 font-bold text-xl p-1">register or login form</p>
                 </div>
+
+
+
+
             </header>
             <Routes>
                 <Route path="/" element={<LogIn/>}/>
